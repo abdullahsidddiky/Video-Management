0c170cf2e14e55b7735c3f4dba9bd7094849d34d
abdullahsidddiky <mohammad.siddiky@northsouth.edu>
2023-03-04 15:50:45 +0600

    mudular based

--- start/routes.ts
+++ start/routes.ts
@@ -18,22 +18,8 @@
 |
 */
 
-import { Request } from '@adonisjs/core/build/standalone'
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
 })
-Route.get('read/:school_id', 'VideoGalleriesController.read')
-Route.post('/create', 'VideoGalleriesController.create')
-Route.put('/update/:school_id', 'VideoGalleriesController.update')
-Route.delete('/delete/:school_id', 'VideoGalleriesController.delete')
-
-Route.post(':id/create_cat/', 'GalleryCategoriesController.create_cat')
-Route.get(':school_id/read_cat', 'GalleryCategoriesController.read_cat')
-
-Route.post(':school_id/:category_id/create_videos', 'GalleryVideosController.create_video')
-Route.post('/search', 'GalleryVideosController.search_video')
-Route.post('/search/:catid', 'GalleryVideosController.search_by_catid')
-Route.put('update_views/:id', 'GalleryVideosController.upate_videos_views')
-Route.get('show_views', 'GalleryVideosController.show_views')
