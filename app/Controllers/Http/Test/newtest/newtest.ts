49d02152dc498775480db04f787588214ee10df8
abdullahsidddiky <mohammad.siddiky@northsouth.edu>
2023-03-05 12:04:52 +0600

    :

--- app/Controllers/Http/Test/newtest/newtest.ts
+++
@@ -1,4 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-Route.group(()=>{
-  Route.get('testnewtest', 'newtest/newtestController.getnewtestByLimit')
-}).prefix('test/newtest').middleware('auth')
